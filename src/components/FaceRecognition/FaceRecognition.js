ef131f3c58f069ed900bc030c4343a997596f054
Shajith Hameed <shajith.sjps@gmail.com>
2022-01-14 18:33:36 +0530

    width changed

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -4,7 +4,7 @@ const FaceRecognition = ({imageUrl,box}) =>{
 	return(
 		<div className='center ma'>
             <div className='absolute mt2'>
-                <img id='inputimage' alt="" src={imageUrl} width='500px' height='auto'/>
+                <img id='inputimage' alt="" src={imageUrl} width='300px' height='auto'/>
                 <div className='bb' style={{top:box.topRow,right:box.rightCol,bottom:box.bottomRow,left:box.leftCol}}></div>
             </div>
             
